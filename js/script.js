6b6be27fa1567db10a7074766a2d3dd663ffcd1a
Vinicius Ventura <vinicius__20@hotmail.com>
2020-06-04 19:19:17 -0300

    background and style application, layout formatting, bug fix (snake disappeared from box area)

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let food = {
 }
 
 function createBG() {
-  context.fillStyle = "lightblue";
+  context.fillStyle = "#ebebe0";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
@@ -39,10 +39,10 @@ function update(event) {
 }
 
 function startGame() {
-  if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
-  if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-  if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
-  if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+  if (snake[0].x == 15 * box && direction == 'right') snake[0].x = 0;
+  if (snake[0].x == 0 && direction == 'left') snake[0].x = 16 * box;
+  if (snake[0].y == 15 * box && direction == 'down') snake[0].y = 0;
+  if (snake[0].y == 0 && direction == 'up') snake[0].y = 16 * box;
 
   for (i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
